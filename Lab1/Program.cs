737a75f2ae1fe600a6f5bae30b083abfcd30a29e
KevinTri-c <157545226+KevinTri-c@users.noreply.github.com>
2025-03-21 20:20:02 -0400

    Update Program.cs

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-    }
+       // Step 1: Basic Input
+        Console.WriteLine("Name: Kevin Valcarcel");
+        Console.WriteLine("Course Title: IT-1050 Lab 1");
+            
+       /* Step 2: Use Comments 
+       Name: Kevin Valcarcel 
+       Title: IT-1050 - Lab 1 
+        */
+
+       // Step 3: Declare and Use Variables
+        int favoritenumber = 3712; 
+        string favoritelanguage = "C#";
+        double numberofprograms = 1.0;
+        bool programmingexperience = false;
+        Console.WriteLine("My favorite number is" + favoritenumber);
+        Console.WriteLine("My favorite programming language is now" + favoritelanguage + "becuase it is the one I am learning currently");
+        Console.WriteLine("The number of programs I have written is now" + numberofprograms);
+        Console.WriteLine("Any Programming Experience" + programmingexperience);
+
+        // Step 4: Use Constants
+        cont string schoolname = "Cuyahoga Community College"
+            Console.WriteLine("The name of my place of learning is" + schoolname);
+    } 
 }
