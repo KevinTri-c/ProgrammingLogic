8a3da5db2a44219c8f4a9ba1e395271d2af4dec8
KevinTri-c <157545226+KevinTri-c@users.noreply.github.com>
2025-04-12 06:46:03 +0000

    Lab 3 finished

--- Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52aae7c6002c8ef3f8c31758bac9b8d600531794")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26da2199bd50477dd23fd4390c7c107cd5e28d51")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lab3/Program.cs
@@ -0,0 +1,104 @@
+﻿using System.ComponentModel;
+
+namespace Lab3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Step 1 operator practice
+          int a = 10;
+          int b = 5;
+          int c = 15;
+          Console.WriteLine(a > b);
+          Console.WriteLine(a < c);
+          Console.WriteLine(a > b && a > c);
+          
+          //Step 2 boolean logic
+          bool isRaining = true;
+          bool haveUmbrella = false;
+
+           if (isRaining && !haveUmbrella)
+        {
+            Console.WriteLine("Take an umbrella!");
+        }
+        else
+        {
+            Console.WriteLine("You're good to go!");
+        }
+        // Step 3 Conditional Logic – Movie Ticket Pricing
+
+        Console.Write("Enter your age: ");
+        string input = Console.ReadLine();
+        int age;
+
+        if (int.TryParse(input, out age))
+        {
+            if (age < 5)
+            {
+                Console.WriteLine("Ticket is free!");
+            }
+            else if (age >= 5 && age <= 12)
+            {
+                Console.WriteLine("Child ticket: $5");
+            }
+            else if (age >= 13 && age <= 64)
+            {
+                Console.WriteLine("Standard ticket: $10");
+            }
+            else // age >= 65
+            {
+                Console.WriteLine("Senior ticket: $6");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number.");
+        }
+        // step 4 Using a sqwitch statement
+
+ Console.Write("Enter a day of the week (1-7): ");
+        string userinput = Console.ReadLine();
+        int dayNumber;
+
+        if (int.TryParse(userinput, out dayNumber))
+        {
+            switch (dayNumber)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Invalid number. Please enter a number between 1 and 7.");
+                    break;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a number.");
+        }
+        
+
+          
+           
+          
+    }
+}      
