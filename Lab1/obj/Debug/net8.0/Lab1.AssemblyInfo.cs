fcc6e96d19e81f560ef98982643b7c64f122ba41
KevinTri-c <157545226+KevinTri-c@users.noreply.github.com>
2025-04-12 06:48:51 +0000

    lab 3 finished

--- Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26da2199bd50477dd23fd4390c7c107cd5e28d51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a3da5db2a44219c8f4a9ba1e395271d2af4dec8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
